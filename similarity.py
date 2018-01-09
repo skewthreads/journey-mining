de978a9c41a4fa8d3e649835e1845f582186dde6
Thanos Giannopoulos <thanosgn@gmail.com>
2018-01-09 17:41:37 +0200

    Added LCSS algorithm

--- similarity.py
+++ similarity.py
@@ -13,16 +13,23 @@ def DTWDistance(t1, t2):
     dtw[0][0] = 0
     for i in range(1,n+1):
         for j in range(1,m+1):
-            point1 = t1[i-1]
-            point2 = t2[j-1]
-            lon1 = point1[1]
-            lat1 = point1[2]
-            lon2 = point2[1]
-            lat2 = point2[2]
-            cost = haversine(lon1, lat1, lon2, lat2)
+            cost = haversine(t1[i-1][1], t1[i-1][2], t2[j-1][1], t2[j-1][2])
             dtw[i][j] = cost + min(dtw[i-1][j], dtw[i][j-1], dtw[i-1][j-1])
     return dtw[n][m]
 
+def LCSLength(t1, t2):
+    n = len(t1)
+    m = len(t2)
+    # An (n+1) times (m+1) matrix
+    C = [[0] * (m + 1) for _ in range(n + 1)]
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            distance = haversine(t1[i-1][1], t1[i-1][2], t2[j-1][1], t2[j-1][2])
+            if distance < 0.2:
+                C[i][j] = C[i-1][j-1] + 1
+            else:
+                C[i][j] = max(C[i][j-1], C[i-1][j])
+    return C[n][m]
 
 def find_neighbors(k):
     with open('datasets/test_set_a1.csv', 'r') as inputFile, open('datasets/tripsClean.csv', 'r') as inputClean:
