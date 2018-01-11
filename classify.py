91a3382f028143b6e40bf56a91f07beb2ba407dd
Dimitris Mouris <jimouris@gmail.com>
2018-01-11 17:53:32 +0200

    Return only minimum lat-long for the grid

--- classify.py
+++ classify.py
@@ -1,8 +1,8 @@
 import csv
 import ast
 
-# Find maximum and minimum borders in dataset
-def find_borders(inputFile):
+# Find minimum borders in dataset
+def find_min_boarder(inputFile):
     with open(inputFile, 'r') as inFile:
         inputReader = csv.reader(inFile, delimiter=';')
         min_lon = float('inf')
@@ -17,16 +17,8 @@ def find_borders(inputFile):
             temp = min(timeseries, key=lambda x: x[2])
             if min_lat > temp[2]:
                 min_lat = temp[2]
-            temp = max(timeseries, key=lambda x: x[1])
-            if max_lon < temp[1]:
-                max_lon = temp[1]
-            temp = max(timeseries, key=lambda x: x[2])
-            if max_lat < temp[2]:
-                max_lat = temp[2]
-        return (min_lon, min_lat), (max_lon, max_lat)
+        return min_lon, min_lat
 
 
-
-(min_lon, min_lat), (max_lon, max_lat) = find_borders('datasets/tripsClean.csv')
+min_lon, min_lat = find_min_boarder('datasets/tripsClean.csv')
 print "min: ", (min_lon, min_lat)
-print "max: ", (max_lon, max_lat)
