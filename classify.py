a59e229e41778d86a72bbc4988908e38bdb21c0b
Dimitris Mouris <jimouris@gmail.com>
2018-01-31 21:26:34 +0200

    Add classifiers

--- classify.py
+++ classify.py
@@ -1,7 +1,22 @@
 import csv
 import ast
+import pandas as pd
+from sklearn import preprocessing
 from orderedset import OrderedSet
 from preprocess import haversine
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import TfidfTransformer
+import numpy as np
+import sys
+
 
 # Find minimum borders in dataset
 def find_min_border(inputFile):
@@ -33,7 +48,6 @@ def gridify(filename, (min_lon, min_lat), cellSide): #cellSide in km
                 timeseries = ast.literal_eval(row[2])
             else: #elif len(row) == 2
                 timeseries = ast.literal_eval(row[1])
-
             cellMap = OrderedSet()
             for point in timeseries:
                 dy = haversine(float(point[1]), min_lat, min_lon, min_lat)
@@ -44,9 +58,43 @@ def gridify(filename, (min_lon, min_lat), cellSide): #cellSide in km
             else: #elif len(row) == 2
                 outputWriter.writerow([tripID, ';'.join([str(x) for x in cellMap])])
 
+def classify(classifier):
+    df=pd.read_csv('datasets/tripsClean_grid.csv',sep='!')
+    le = preprocessing.LabelEncoder()
+    le.fit(df['JourneyPatternID'])
+    Y_train=le.transform(df['JourneyPatternID'])
+    X_train=df['Trajectory']
+    vectorizer = HashingVectorizer(binary=True, tokenizer=lambda x: x.split(';'))
+    pipeline = Pipeline([
+        ('vect', vectorizer),
+        ('classifier', classifier)
+    ])
+    kf = KFold(n_splits=10)
+    mean_accuracy = 0
+    mean_tpr = 0.0
+    mean_fpr = np.linspace(0, 1, 100)
+    all_tpr = []
+    fold = 0
+    for train_index, test_index in kf.split(X_train):
+    	X_train2, X_test = X_train[train_index], X_train[test_index]
+    	Y_train2, Y_test = Y_train[train_index], Y_train[test_index]
+    	pipeline.fit(X_train2,Y_train2)
+    	predicted=pipeline.predict(X_test)
+    	acc = accuracy_score(Y_test,predicted)
+    	mean_accuracy += acc
+    	fold += 1
+    mean_accuracy = mean_accuracy / fold
+    print "Mean accuracy: ",mean_accuracy
 
 
+# min_lon, min_lat = (-6.61505, 53.07045)
 min_lon, min_lat = find_min_border('datasets/tripsClean.csv')
-print "min: ", (min_lon, min_lat)
-gridify('datasets/tripsClean.csv',(min_lon, min_lat), 1.5)
-gridify('datasets/test_set.csv',(min_lon, min_lat), 1.5)
+print 'min: ', (min_lon, min_lat)
+# gridify('datasets/tripsClean.csv',(min_lon, min_lat), float(sys.argv[1]))
+gridify('datasets/tripsClean.csv',(min_lon, min_lat), 1)
+gridify('datasets/test_set.csv',(min_lon, min_lat), 1)
+
+classify(classifier = KNeighborsClassifier())
+# classify(classifier = LogisticRegression())
+# classify(classifier = RandomForestClassifier(n_estimators = 10, random_state = 1, n_jobs=-1))
+
